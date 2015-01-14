735d6737ee4e352c1c39d941f51f7b4c16b8e7a0
tonyfransis <tony.612.4u@gmail.com>
2015-01-14 10:54:40 +0530

    added mongo support and pipline support

--- garageclothing/pipelines.py
+++ garageclothing/pipelines.py
@@ -5,7 +5,13 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from pymongo import MongoClient
 
 class GarageclothingPipeline(object):
+
+    def __init__(self):
+        self.db = MongoClient('127.0.0.1').garageclothingdata
+
     def process_item(self, item, spider):
+        self.db.product_info.insert(dict(item))
         return item
